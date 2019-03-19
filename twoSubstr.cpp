a173b092845ab1488eceaee74f6d1de2e6dc1da1
dthinkcs <deliberatethink@gmail.com>
2019-03-19 08:13:54 +0530

    strproblem 1

--- twoSubstr.cpp
+++ twoSubstr.cpp
@@ -1,7 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int findMy(string s, string sub)
+{
 
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == sub[0])
+        {
+            int j = 0;
+            for (int inner_i = i; j < sub.length(); j++, inner_i++)
+                if (s[inner_i] != sub[j])
+                    break;
+
+
+            if (j == sub.length())
+                return i;
+
+            //j = 0;
+        }
+
+    }
+    return -1;
+}
+
+void twoSubstrings(string s)
+{
+	int indexAB = findMy(s, "AB");
+    if (indexAB == -1)
+    {
+        cout << "no" << endl;
+        return;
+    }
+
+    int indexBA = findMy(s, "BA");
+    if (indexBA == -1)
+    {
+        cout << "no" << endl;
+        return;
+    }
+    // difference between 2 indices must be 2 or greater
+    if (abs(indexAB - indexBA) < 2)
+    {
+        cout << "no" << endl;
+
+    }
+    else
+        cout << "yes" << endl;
+
+}
 
 void twoSubstrings1( string s)
 {
